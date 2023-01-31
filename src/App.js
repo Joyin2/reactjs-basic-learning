a9e4612811921bc995505b7a3abc55f24f481462
Joyin2 <joyinlaskar2000@gmail.com>
2023-01-31 22:34:03 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
-
+import Home from "./pages/Home";
+import {Routes, Route} from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Detail from "./pages/Detail"
+import AddEditBlog from "./pages/AddEditBlog"
+import About from "./pages/About"
 function App() {
   return (
-    <div className="App" >
-      hello
+    <div >
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/detail/:id" element={<Detail/>} />
+        <Route path="/create" element={<AddEditBlog/>} />
+        <Route path="/update/:id" element={<AddEditBlog/>} />
+        <Route path="/about" element={<About/>} />
+
+
+        
+      </Routes>
+      
     </div>
   );
 }
---
+++ src/pages/AddEditBlog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddEditBlog = () => {
+  return (
+    <div>
+      Add Edit
+    </div>
+  )
+}
+
+export default AddEditBlog
